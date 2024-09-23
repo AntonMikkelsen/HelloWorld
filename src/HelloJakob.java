73fb271b9bbbaceb98fa3c7d74f7f889fdb766b1
AntonMikkelsen <anto0004@stud.kea.dk>
2024-09-23 12:27:42 +0200

    First commit

---
+++ src/HelloJakob.java
@@ -0,0 +1,22 @@
+public class HelloJakob {
+    public static void main(String[] args) {
+
+
+        int studentAge = 22;
+        double studentKarakter = 11.2;
+        char studentInitial = 'A';
+        char studentLastInitial = 'M';
+        boolean studentPerfectAttendance = false;
+        String studentName = "Anton Mikkelsen";
+
+
+        System.out.println(studentAge);
+        System.out.println(studentKarakter);
+        System.out.println(studentInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(studentPerfectAttendance);
+        System.out.println(studentName);
+        System.out.println(studentName + " har et karakter gennemsnit på " + studentKarakter);
+
+    }
+}
